2ea350fc5d686430b993f2d7a5e906513d3cf978
Christopher Weems <hello@christopherweems.com>
2020-05-16 19:45:35 -0400

    Add Array.split(intoGroupsOf:)

--- Sources/unstandard/Extensions/Array+Extensions.swift
+++ Sources/unstandard/Extensions/Array+Extensions.swift
@@ -53,3 +53,46 @@ public extension Array where Element: Hashable {
     }
 }
 
+
+// MARK: - Splitting into groups
+
+enum CollectionSplittingStrategy: Equatable {
+    case fillBackward
+    // ^^ Array of 42 elements split by 20: [20], [20], [2]
+    
+    indirect case evenly(fallback: CollectionSplittingStrategy)
+    // ^^ Array of 63 elements split by 20 with fill backward fallback: [16, 16, 16, 15]
+    //    evenly case as a fallback is a programming error
+    
+}
+
+
+extension Array {
+    func split(intoGroupsOf maxCountPerBucket: Int, splittingStrategy: CollectionSplittingStrategy = .fillBackward) -> Array<[Element]> {
+        assert(splittingStrategy == .fillBackward, "Only .fillBackward strategy implemented")
+        
+        var containers = Array<[Element]>()
+        
+        var elementCountInThisBucket = 0
+        
+        self.forEach {
+            if maxCountPerBucket == elementCountInThisBucket {
+                elementCountInThisBucket = 0
+            }
+            
+            if elementCountInThisBucket == 0 {
+                containers.append([Element]())
+            }
+            
+            var last = containers.popLast()!
+            last.append($0)
+            
+            elementCountInThisBucket += 1
+            containers.append(last)
+            
+        }
+        
+        return containers
+    }
+}
+
