fce1ed9a3466a13f38ff9602fd8205e4eb393985
Christopher Weems <hello@christopherweems.com>
2020-05-22 13:22:14 -0400

    Add CGPoint `+` `-` operators

--- Sources/unstandard/Extensions/CGPoint+Extensions.swift
+++ Sources/unstandard/Extensions/CGPoint+Extensions.swift
@@ -9,6 +9,16 @@
 import CoreGraphics
 
 public extension CGPoint {
+    static func +(_ lhs: CGPoint, _ rhs: CGPoint) -> CGPoint {
+        .init(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+    
+    static func -(_ lhs: CGPoint, _ rhs: CGPoint) -> CGPoint {
+        .init(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
+}
+
+public extension CGPoint {
     func asCGSize() -> CGSize {
         .init(width: x, height: y)
     }
