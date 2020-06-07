3113b09aa1dacaa729da5f78d51ac83d9e10adb9
Christopher Weems <hello@christopherweems.com>
2020-06-06 20:41:26 -0400

    Add StringProtocol.leftPadding(toLength:withPad:shouldTruncate:)

--- Sources/unstandard/Extensions/Protocol Extensions/StringProtocol+Extensions.swift
+++ Sources/unstandard/Extensions/Protocol Extensions/StringProtocol+Extensions.swift
@@ -19,11 +19,34 @@ public extension StringProtocol {
 }
 
 
+// MARK: - .filtered(by:)
+
 public extension StringProtocol {
     func filtered(by filterSet: CharacterSet) -> String {
         self.unicodeScalars
             .filter(filterSet.contains)
             .map { String($0) }
             .joined()
+    }
+}
+
+
+// MARK: - .leftPadding(...)
+
+public extension StringProtocol {
+    func leftPadding(toLength: Int, withPad character: Character, shouldTruncate: Bool = false) -> String {
+        let newLength = self.count
+        
+        switch newLength {
+        case ..<toLength:
+            return String(repeatElement(character, count: toLength - newLength)) + self
+            
+        case _ where shouldTruncate:
+            return self[index(self.startIndex, offsetBy: newLength - toLength)...].asString()
+            
+        default:
+            return self.asString()
+            
+        }
     }
 }
