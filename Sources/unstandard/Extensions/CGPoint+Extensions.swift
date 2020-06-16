656438165cf8d4a73200914d8a3e5032dc61657a
Christopher Weems <hello@christopherweems.com>
2020-06-16 14:11:44 -0400

    Add CGPoint.magnitude

--- Sources/unstandard/Extensions/CGPoint+Extensions.swift
+++ Sources/unstandard/Extensions/CGPoint+Extensions.swift
@@ -8,6 +8,9 @@
 #if canImport(CoreGraphics)
 import CoreGraphics
 
+
+// MARK: - Operators
+
 public extension CGPoint {
     static func +(_ lhs: CGPoint, _ rhs: CGPoint) -> CGPoint {
         .init(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
@@ -17,6 +20,23 @@ public extension CGPoint {
         .init(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
     }
 }
+
+
+// MARK: - Magnitude
+
+public extension CGPoint {
+    var magnitude: CGFloat {
+        (x * x + y * y).squareRoot()
+    }
+}
+
+public extension Optional where Wrapped == CGPoint {
+    var magnitude: CGFloat {
+        self?.magnitude ?? 0.0
+    }
+}
+
+// MARK: - Conversions
 
 public extension CGPoint {
     func asCGSize() -> CGSize {
