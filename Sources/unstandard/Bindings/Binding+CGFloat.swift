3f2bce7be86bd172e19fc041641a866ac5899259
Christopher Weems <hello@christopherweems.com>
2020-06-16 18:46:24 -0400

    Fix Binding+CGFloat to require can import CoreGraphics & SwiftUI

--- Sources/unstandard/Bindings/Binding+CGFloat.swift
+++ Sources/unstandard/Bindings/Binding+CGFloat.swift
@@ -5,9 +5,14 @@
 //  Created by Christopher Weems on 6/16/20.
 //
 
+#if canImport(CoreGraphics) && canImport(SwiftUI)
 import Foundation
+import CoreGraphics
+import SwiftUI
 
 public extension Binding where Value == CGFloat? {
     var isNil: Bool { wrappedValue == nil }
     
 }
+
+#endif
