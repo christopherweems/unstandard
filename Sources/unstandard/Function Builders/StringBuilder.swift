1352a1cbaf10702d3a3e3e30b230086dc9413627
Christopher Weems <hello@christopherweems.com>
2020-06-30 16:18:18 -0400

    Add `StringBuilder` function builder

---
+++ Sources/unstandard/Function Builders/StringBuilder.swift
@@ -0,0 +1,49 @@
+//
+//  StringBuilder.swift
+//  
+//
+//  Created by Christopher Weems on 6/30/20.
+//
+
+import Foundation
+
+
+/// Usage: see `unstandardTests.testStringBuilder()`
+
+
+// MARK: - Separators
+
+public protocol StringBuilderJoinSeparator {
+    static var separator: String { get }
+}
+
+public struct NewLine: StringBuilderJoinSeparator {
+    public static var separator: String { "\n" }
+    
+}
+
+
+// MARK: - StringBuilder
+
+@_functionBuilder
+public struct StringBuilder<Join> where Join : StringBuilderJoinSeparator {
+    public static func buildBlock() -> String {
+        ""
+    }
+    
+    public static func buildBlock(_ contents: String...) -> String {
+        contents.joined(separator: Join.separator)
+    }
+    
+    public static func buildOptional(_ contents: String?) -> String {
+        contents ?? ""
+    }
+    
+    public static func buildEither(first: String) -> String {
+        first
+    }
+    
+    public static func buildEither(second: String) -> String {
+        second
+    }
+}
