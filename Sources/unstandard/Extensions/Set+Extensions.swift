7d881463d4cc05d19bc39310c5fde37f6a382145
Christopher Weems <hello@christopherweems.com>
2020-07-01 17:12:09 -0400

    Add variadic Set.init(..)

---
+++ Sources/unstandard/Extensions/Set+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Set+Extensions.swift
+//
+//
+//  Created by Christopher Weems on 7/1/20.
+//
+
+import Foundation
+
+public extension Set {
+    init(_ elements: Element...) {
+        self = .init(elements)
+    }
+}
