805acbddefe76353511d4918c3f5239fcd32de60
Christopher Weems <hello@christopherweems.com>
2020-07-13 11:30:48 -0400

    Add CharacterSet  operator

--- Sources/unstandard/Extensions/CharacterSet+Extensions.swift
+++ Sources/unstandard/Extensions/CharacterSet+Extensions.swift
@@ -11,4 +11,9 @@ public extension CharacterSet {
     static func +(_ lhs: CharacterSet, _ rhs: String) -> CharacterSet {
         lhs.union(.init(charactersIn: rhs))
     }
+    
+    static func -(lhs: CharacterSet, rhs: String) -> CharacterSet {
+        let removeSet = CharacterSet(charactersIn: rhs)
+        return lhs.subtracting(removeSet)
+    }
 }
