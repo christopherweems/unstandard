06ecae83f6611518c8f736523761e3cf650ce69c
Christopher Weems <hello@christopherweems.com>
2020-07-20 13:58:15 -0400

    Fix access control

--- Sources/unstandard/Extensions/Range+Extensions.swift
+++ Sources/unstandard/Extensions/Range+Extensions.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-extension Range where Bound : SignedNumeric {
+public extension Range where Bound : SignedNumeric {
     static var zero: Self { (.zero)..<(.zero) }
     
 }
 
-extension Range where Bound: FloatingPoint {
+public extension Range where Bound: FloatingPoint {
     static func /(_ lhs: Self, _ rhs: Bound) -> Range {
         (lhs.lowerBound/rhs)..<(lhs.upperBound/rhs)
     }
