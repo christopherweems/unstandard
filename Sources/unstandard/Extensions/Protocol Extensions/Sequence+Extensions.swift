7cc1d950967f3d5b1fa7c4d918f14906a149427a
Christopher Weems <hello@christopherweems.com>
2020-07-30 11:30:39 -0400

    Add Sequence.stableSorted(..)

--- Sources/unstandard/Extensions/Protocol Extensions/Sequence+Extensions.swift
+++ Sources/unstandard/Extensions/Protocol Extensions/Sequence+Extensions.swift
@@ -24,3 +24,23 @@ public extension Sequence {
         return nil
     }
 }
+
+
+// MARK: - Stable Sort
+
+public extension Sequence {
+    func stableSorted(by areInIncreasingOrder: (Element, Element) throws -> Bool) rethrows -> [Element] {
+        try enumerated()
+            .sorted { a, b -> Bool in
+                try areInIncreasingOrder(a.element, b.element) ||
+                    (a.offset < b.offset && !areInIncreasingOrder(b.element, a.element))
+            }
+            .map { $0.element }
+    }
+}
+
+public extension Sequence where Element : Comparable {
+    func stableSorted() -> [Element] {
+        self.stableSorted(by: { $0 < $1 })
+    }
+}
