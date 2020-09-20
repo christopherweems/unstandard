86c5a34248e8dff41e8d4d0cc1eab46e3348585e
Christopher Weems <hello@christopherweems.com>
2020-09-19 21:31:53 -0400

    Add `DateFormatter.withConfiguration(_:)`

---
+++ Sources/unstandard/Extensions/DateFormatter+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  DateFormatter+Extensions.swift
+//  
+//
+//  Created by Christopher Weems on 9/19/20.
+//
+
+import Foundation
+
+public extension DateFormatter {
+    static func withConfiguration(_ configurationHandler: (Self) -> Void) -> Self {
+        let new = Self.init()
+        configurationHandler(new)
+        return new
+    }
+    
+}
