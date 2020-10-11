7e2d5527ae7ea9cab6fe87ebee715988558ca618
Christopher Weems <hello@christopherweems.com>
2020-10-11 17:20:16 -0400

    Add `AnyTrue` result builder

---
+++ Sources/unstandard/Result Builders/AnyTrue.swift
@@ -0,0 +1,16 @@
+//
+//  AnyTrue.swift
+//  
+//
+//  Created by Christopher Weems on 10/11/20.
+//
+
+import Foundation
+
+@_functionBuilder
+public struct AnyTrue {
+    static func buildBlock(_ elements: Bool...) -> Bool {
+        elements.lazy.contains(where: \.isTrue)
+    }
+    
+}
