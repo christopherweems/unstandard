541c2b19b78dac28be2d02e9d802de499f719875
Christopher Weems <hello@christopherweems.com>
2020-10-26 11:26:54 -0400

    Add `UUIDResult`

---
+++ Sources/unstandard/Result Builders/UUIDResult.swift
@@ -0,0 +1,28 @@
+//
+//  UUIDResult.swift
+//
+//
+//  Created by Christopher Weems on 9/29/20.
+//  Copyright © 2020 Christopher Weems. All rights reserved.
+//
+
+import Foundation
+
+@_functionBuilder
+public struct UUIDResult {
+    public static func buildBlock(_ uuid: UUID) -> UUID {
+        uuid
+    }
+    
+}
+
+public extension UUIDResult {
+    static func buildExpression(_ uuid: UUID) -> UUID {
+        uuid
+    }
+    
+    static func buildExpression(_ uuid: uuid_t) -> UUID {
+        .init(uuid: uuid)
+    }
+    
+}
