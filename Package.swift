de9e588d215cdc00af6cf965a64fab868ceebdf7
Christopher Weems <hello@christopherweems.com>
2020-12-01 15:47:09 -0500

    Add `triple-q` package dependency

--- Package.swift
+++ Package.swift
@@ -29,6 +29,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/christopherweems/Resultto.git", .upToNextMajor(from: "0.0.1")),
+        .package(url: "https://github.com/christopherweems/triple-q.git", .upToNextMajor(from: "0.0.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -37,6 +38,7 @@ let package = Package(
             name: "unstandard",
             dependencies: [
                 .product(name: "Resultto", package: "Resultto"),
+                .product(name: "triple-q", package: "triple-q"),
             ]),
         .target(
             name: "do_and_throw",
