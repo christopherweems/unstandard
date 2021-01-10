17b9d27758839af6b1466b4a426dc41d81fde2e3
Christopher Weems <hello@christopherweems>
2021-01-10 17:12:56 -0500

    Add `_ListFormatter`

---
+++ Sources/unstandard/Formatters/_ListFormatter.swift
@@ -0,0 +1,31 @@
+//
+//  _ListFormatter.swift
+//  
+//
+//  Created by Christopher Weems on 1/10/21.
+//
+
+import Foundation
+
+public class _ListFormatter {
+    public class func localizedString(byJoining strings: [String]) -> String {
+        if #available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *) {
+            return ListFormatter.localizedString(byJoining: strings)
+        }
+        
+        switch strings.count {
+        case 0:
+            return ""
+            
+        case 1:
+            return strings[0]
+            
+        case 2:
+            return strings[0...1].joined(separator: " & ")
+            
+        default:
+            return strings.prefix(upTo: strings.lastIndex).joined(separator: ", ").appending(", & \(strings.last!)")
+        }
+    }
+    
+}
