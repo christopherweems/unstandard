7864df0d4eadc2b09b23dd09a34b90166f3c1799
Christopher Weems <hello@christopherweems>
2021-01-21 10:39:52 -0500

    Update package url for `swift-algorithms` dependency

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-algorithms", .upToNextMajor(from: "0.0.2")),
+        .package(url: "https://github.com/apple/swift-algorithms.git", .upToNextMajor(from: "0.0.2")),
         .package(url: "https://github.com/christopherweems/Resultto.git", .upToNextMajor(from: "0.0.2")),
         .package(url: "https://github.com/christopherweems/triple-q.git", .upToNextMajor(from: "0.0.1")),
     ],
