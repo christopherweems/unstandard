847cfb3f7b190134f82606ffb33b63c9f0d5bd4a
Christopher Weems <hello@christopherweems>
2021-02-09 13:21:18 -0500

    Add `Character.asString()`

---
+++ Sources/unstandard/Extensions/Character+Extensions.swift
@@ -0,0 +1,11 @@
+//  Character+Extensions.swift
+//  Created by Christopher Weems on 2/9/21
+
+import Foundation
+
+public extension Character {
+    func asString() -> String {
+        String(self)
+    }
+    
+}
