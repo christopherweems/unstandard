72b69c56c6e45c96bcda45946559f1e65d76a01d
Christopher Weems <hello@christopherweems>
2021-02-27 21:37:58 -0500

    Replace (E,E) typed PairBuilder with (A,B)

--- Sources/unstandard/Result Builders/PairBuilder.swift
+++ Sources/unstandard/Result Builders/PairBuilder.swift
@@ -9,8 +9,14 @@ import Foundation
 
 @resultBuilder
 public struct PairBuilder {
+    /*
     public static func buildBlock<E>(_ lhs: E, _ rhs: E) -> (E, E) {
         (lhs, rhs)
+    }
+    */
+    
+    public static func buildBlock<A,B>(_ first: A, _ second: B) -> (A, B) {
+        (first, second)
     }
     
 }
