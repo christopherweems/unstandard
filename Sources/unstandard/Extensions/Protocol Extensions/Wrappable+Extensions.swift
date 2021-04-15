7e83415c903d760b5e3d9e098234af5b1dcbe0de
Christopher Weems <hello@christopherweems>
2021-04-15 15:28:31 -0400

    Rename `Wrappable.assert(_:)` as `Wrappable.assertTrue(_:)`

--- Sources/unstandard/Extensions/Protocol Extensions/Wrappable+Extensions.swift
+++ Sources/unstandard/Extensions/Protocol Extensions/Wrappable+Extensions.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public extension Wrappable {
-    func assert(_ condition: (Self) -> Bool, _ message: @autoclosure () -> String = .init(),
+    func assertTrue(_ condition: (Self) -> Bool, _ message: @autoclosure () -> String = .init(),
                 file: StaticString = #file, line: UInt = #line) -> Self {
         Swift.assert(condition(self), message())
         return self
