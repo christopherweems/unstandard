ea3f22133cb9092321ebc6e86f4db8274ee7ef6c
Christopher Weems <hello@christopherweems.com>
2021-07-27 11:59:04 -0400

    Add `confirm(..)` function

---
+++ Sources/unstandard/Algorithms/Confirm.swift
@@ -0,0 +1,19 @@
+//
+//  Confirm.swift
+//
+//
+//  Created by Christopher Weems on 7/27/21.
+//
+
+import Resultto
+
+public func confirm<E: Error>(throwing failureError: E, @SingleResult predicate: () -> Bool) throws {
+    switch predicate() {
+    case true:
+        break
+        
+    case false:
+        throw failureError
+        
+    }
+}
