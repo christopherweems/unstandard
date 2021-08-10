28822c8e5500b264c4943cc75aa38b02eff3ab3b
Christopher Weems <hello@christopherweems.com>
2021-08-09 21:30:12 -0400

    `ForEach(_:content:)` for ClosedRange<Int> is now deprecated

--- Sources/unstandard/Extensions/ForEach+Extensions.swift
+++ Sources/unstandard/Extensions/ForEach+Extensions.swift
@@ -8,10 +8,12 @@
 #if canImport(SwiftUI)
 import SwiftUI
 
+@available(*, deprecated)
 public extension ForEach where Data == Range<Int>, ID == Int, Content : View {
     init(_ range: ClosedRange<Int>, @ViewBuilder content: @escaping (Int) -> Content) {
-        self.init(range.lowerBound..<(range.upperBound + 1), content: content)
+        self.init(range.lowerBound..<(range.upperBound + 1), id: \.hashValue, content: content)
     }
+    
 }
 
 #endif
