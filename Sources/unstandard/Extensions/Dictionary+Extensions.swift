0927b395c94f3bce76f8a905a970df6e67d31e48
Christopher Weems <hello@christopherweems.com>
2021-09-17 19:48:36 -0400

    Add `Dictionary.sortedValues(on:reversed:)`

--- Sources/unstandard/Extensions/Dictionary+Extensions.swift
+++ Sources/unstandard/Extensions/Dictionary+Extensions.swift
@@ -7,17 +7,32 @@
 
 import Foundation
 
-public extension Dictionary {
-    func first(whereKey keyPredicate: (Key) -> Bool) -> (key: Key, value: Value)? {
+
+// MARK: -
+
+extension Dictionary {
+    public func first(whereKey keyPredicate: (Key) -> Bool) -> (key: Key, value: Value)? {
         return first(where: { keyPredicate($0.key) })
     }
+    
+}
+
+
+// MARK: - Sorting values by key or value
+
+extension Dictionary {
+    public func sortedValues<V>(on sortKeyPath: KeyPath<Element, V>, reversed: Bool = false) -> [Value] where V : Comparable {
+        self.sorted(on: sortKeyPath, comparison: { reversed ? $1 < $0 : $0 < $1 })
+            .map(\.value)
+    }
+    
 }
 
 
 // MARK: -
 
-public extension Dictionary {
-    subscript(key: Key, initialValue initialValue: @autoclosure () -> Value) -> Value {
+extension Dictionary {
+    public subscript(key: Key, initialValue initialValue: @autoclosure () -> Value) -> Value {
         mutating get {
             if let existing = self[key] {
                 return existing
@@ -39,10 +54,9 @@ public extension Dictionary {
 
 // MARK: -
 
-public extension Dictionary {
-    init(uniqueValuesWithKeys valuesWithKeys: [(Value, Key)]) {
+extension Dictionary {
+    public init(uniqueValuesWithKeys valuesWithKeys: [(Value, Key)]) {
         self.init(uniqueKeysWithValues: valuesWithKeys.map { ($0.1, $0.0) })
-        
     }
     
 }
@@ -50,8 +64,8 @@ public extension Dictionary {
 
 // MARK: -
 
-public extension Dictionary {
-    init(@ProtocolTypedArrayBuilder<Self> _ builder: () -> [Dictionary<Key, Value>]) {
+extension Dictionary {
+    public init(@ProtocolTypedArrayBuilder<Self> _ builder: () -> [Dictionary<Key, Value>]) {
         let keysAndValues = builder().flatMap { $0.map { ($0.key, $0.value) } }
         self.init(keysAndValues, uniquingKeysWith: { _, _ in fatalError() })
     }
