b151ea38a5736a804d0d6266b9dc0a75f41b3122
Christopher Weems <hello@christopherweems.com>
2021-09-17 21:57:23 -0400

    Add `Optional.toggle(activeValue:)` which toggles between nil & the specified active value each time the method is called

--- Sources/unstandard/Extensions/Optional+Extensions.swift
+++ Sources/unstandard/Extensions/Optional+Extensions.swift
@@ -3,6 +3,32 @@
 
 import Foundation
 
+
+// MARK: -
+
+extension Optional {
+    // Toggles between nil and the active value each time the method is called
+    public mutating func toggle(activeValue: Wrapped) where Wrapped : Equatable {
+        switch self {
+        case let .some(existing) where existing == activeValue:
+            self = nil
+            
+        case .some:
+            // the existing active value wasn't the same as the specified active value,
+            // AMBIGUITY BREAKER: replace it instead of simply clearing it
+            fallthrough
+            
+        case .none:
+            self = activeValue
+            
+        }
+    }
+    
+}
+
+
+// MARK: -
+
 extension Optional {
     public init(if predicate: Bool, _ wrappedValue: () -> Wrapped) {
         switch predicate {
