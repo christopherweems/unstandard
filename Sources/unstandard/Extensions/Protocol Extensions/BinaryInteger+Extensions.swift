8345601ddef51731ef6a3038ee19fd5df2e1a9dc
Christopher Weems <hello@christopherweems.com>
2021-10-07 12:33:24 -0400

    [gardening]

--- Sources/unstandard/Extensions/Protocol Extensions/BinaryInteger+Extensions.swift
+++ Sources/unstandard/Extensions/Protocol Extensions/BinaryInteger+Extensions.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-public extension BinaryInteger {
-    func bounded(within range: ClosedRange<Self>) -> Self {
+extension BinaryInteger {
+    public func bounded(within range: ClosedRange<Self>) -> Self {
         switch self {
         case (...range.lowerBound):
             return range.lowerBound
@@ -21,19 +21,19 @@ public extension BinaryInteger {
         }
     }
     
-    mutating func bound(within range: ClosedRange<Self>) {
+    public mutating func bound(within range: ClosedRange<Self>) {
         self = self.bounded(within: range)
         
     }
     
 }
 
-public extension BinaryInteger {
-    func isBounded(within range: ClosedRange<Self>) -> Bool {
+extension BinaryInteger {
+    public func isBounded(within range: ClosedRange<Self>) -> Bool {
         range.contains(self)
     }
     
-    func isBounded(within range: Range<Self>) -> Bool {
+    public func isBounded(within range: Range<Self>) -> Bool {
         range.contains(self)
     }
     
