f228b2f9aa28e6763b320a3a983224abde86b4b8
Christopher Weems <hello@christopherweems.com>
2021-10-30 19:18:14 -0400

    Add `Character.isNonWhitespace`

---
+++ Sources/unstandardStrings/Character/Character+Classification.swift
@@ -0,0 +1,13 @@
+//
+//  Character+Classification.swift
+//  
+//
+//  Created by Christopher Weems on 10/30/21.
+//
+
+import Foundation
+
+extension Character {
+    public var isNonWhitespace: Bool { self.isWhitespace == false }
+    
+}
