1b4e0e84883d75009f0d087fdac66fd0efee0f01
Christopher Weems <hello@christopherweems.com>
2021-10-31 02:06:00 -0400

    Move `StringProtocol.asString()` to `unstandardStrings` target

--- Sources/unstandard/Extensions/Protocol Extensions/StringProtocol+Extensions.swift
+++ Sources/unstandard/Extensions/Protocol Extensions/StringProtocol+Extensions.swift
@@ -18,19 +18,6 @@ extension StringProtocol {
 }
 
 extension StringProtocol {
-    @inlinable
-    public func asString() -> String {
-        if let string = self as? String {
-            return string
-        
-        }
-        
-        return .init(self)
-    }
-    
-}
-
-extension StringProtocol {
     public func asDouble() -> Double? {
         Double(self)
     }
