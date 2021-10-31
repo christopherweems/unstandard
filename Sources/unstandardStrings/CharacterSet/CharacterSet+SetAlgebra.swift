87fd90a55834a7e94a420ff1480ef058d36be5f7
Christopher Weems <hello@christopherweems.com>
2021-10-31 02:23:55 -0400

    Add `CharacterSet.contains(_:Unicode.Scalar?)`

--- Sources/unstandardStrings/CharacterSet/CharacterSet+SetAlgebra.swift
+++ Sources/unstandardStrings/CharacterSet/CharacterSet+SetAlgebra.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 extension CharacterSet {
+    public func contains(_ member: Unicode.Scalar?) -> Bool {
+        guard let member = member else { return false }
+        return self.contains(member)
+    }
+    
     internal func contains(_unicodeScalarsOf character: Character) -> Bool {
         character.unicodeScalars.allSatisfy(self.contains(_:))
     }
