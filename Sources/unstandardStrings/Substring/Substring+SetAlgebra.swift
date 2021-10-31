c0bed11dfd3b0d9866221115eaeb4b125bb2aa8a
Christopher Weems <hello@christopherweems.com>
2021-10-31 02:12:19 -0400

    Add `Substring.overlaps(_:Substring)`

---
+++ Sources/unstandardStrings/Substring/Substring+SetAlgebra.swift
@@ -0,0 +1,16 @@
+//
+//  Substring+SetAlgebra.swift
+//
+//
+//  Created by Christopher Weems on 9/28/21.
+//
+
+import Foundation
+
+extension Substring {
+    public func overlaps(_ other: Substring) -> Bool {
+        precondition(self.base == other.base, "Substrings must me from same base string")
+        return self.fullRange.overlaps(other.fullRange)
+    }
+    
+}
