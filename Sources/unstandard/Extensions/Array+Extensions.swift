4f19ed5dcb6124a1eba29d340f3efcc762260510
Christopher Weems <hello@christopherweems.com>
2021-10-31 02:23:11 -0400

    Add `Array.init(_:_:)` for chaining two sequences

--- Sources/unstandard/Extensions/Array+Extensions.swift
+++ Sources/unstandard/Extensions/Array+Extensions.swift
@@ -5,6 +5,7 @@
 //  Created by Christopher Weems on 5/22/17.
 //
 
+import Algorithms
 import Foundation
 
 extension Array {
@@ -246,6 +247,21 @@ extension Array {
     
 }
 
+
+// MARK: - Combining two sequences
+
+extension Array {
+    public init<S1, S2>(_ head: S1, _ tail: S2) where S1 : Sequence, S2 : Sequence, S1.Element == Self.Element, S1.Element == S2.Element {
+        let contents = chain(head, tail)
+        self.init(contents)
+    }
+    
+    public init<Tail>(_ head: Element, _ tail: Tail) where Tail : Sequence, Tail.Element == Element {
+        let contents = chain([head], tail)
+        self.init(contents)
+    }
+    
+}
 
 
 // MARK: - Count of elements matching predicate
