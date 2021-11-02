4c3bbe75c9534ef5b77f893a90a8845addb3c366
Christopher Weems <hello@christopherweems.com>
2021-11-02 17:09:44 -0400

    [commented out] Add `StringProtocol.map(_:)`

---
+++ Sources/unstandardStrings/StringProtocol/StringProtocol+Map.swift
@@ -0,0 +1,15 @@
+//
+//  StringProtocol+Map.swift
+//  
+//
+//  Created by Christopher Weems on 11/2/21.
+//
+
+import Foundation
+
+//extension StringProtocol {
+//    public func map(@ProtocolTypedArrayBuilder<Self> _ transform: (Self) -> [Self]) -> String {
+//        transform(self).joined()
+//    }
+//    
+//}
