2c424517fcac52cd753c45484a3718ab39246d5a
Christopher Weems <hello@christopherweems.com>
2021-11-04 14:48:59 -0400

    Add `StringProtocol.formatted(_:)`

---
+++ Sources/unstandardStrings/StringProtocol/StringProtocol+Formatting.swift
@@ -0,0 +1,39 @@
+//
+//  StringProtocol+Formatting.swift
+//  
+//
+//  Created by Christopher Weems on 11/4/21.
+//
+
+import Foundation
+import unstandardCollections
+
+extension StringProtocol {
+    public func formatted(_ formatting: _StringFormatting) -> String {
+        let joinSeparator: String
+        var components: [String] = self.components(separatedBy: CharacterSet.whitespaces + "-")
+        guard !components.isEmpty else { return "" }
+        
+        switch formatting {
+        case .camelCase:
+            joinSeparator = ""
+            components.replace(at: \.startIndex, with: components.first!.lowercased())
+            components.replaceSubrange(\.tailRange) { $0.capitalized }
+            
+        case .hyphenated:
+            joinSeparator = "-"
+            components = components.map { $0.lowercased() }
+            
+        }
+        
+        return components.joined(separator: joinSeparator)
+    }
+    
+    
+}
+
+public enum _StringFormatting {
+    case camelCase
+    case hyphenated
+    
+}
