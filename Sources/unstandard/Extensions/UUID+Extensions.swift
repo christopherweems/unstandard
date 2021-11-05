b235afb4ec5bccb0697dfbea32b2cf8554b99cfb
Christopher Weems <hello@christopherweems.com>
2021-11-05 14:52:27 -0400

    Add `UUID.String`

--- Sources/unstandard/Extensions/UUID+Extensions.swift
+++ Sources/unstandard/Extensions/UUID+Extensions.swift
@@ -14,3 +14,9 @@ extension UUID {
     }
     
 }
+
+/// Usage: `UUID.String` can be used in declarations to specify what kind of String the function expects
+extension UUID {
+    public typealias String = Swift.String
+    
+}
