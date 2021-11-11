1b4e7c2964775d281cd38e7a45f601a926c7d77b
Christopher Weems <hello@christopherweems.com>
2021-11-11 09:33:03 -0500

    Add `Range.enumerateRanges(_anchoredAt:)`

--- Sources/unstandard/Extensions/Range+Extensions.swift
+++ Sources/unstandard/Extensions/Range+Extensions.swift
@@ -30,3 +30,35 @@ public extension Range {
     }
     
 }
+
+
+// MARK: - `Range.enumerateRanges(anchoredAt:)`
+
+public enum RangePosition {
+    case start
+    case end
+    
+}
+
+extension Range where Bound : Strideable, Bound.Stride : SignedInteger, Bound.Stride.Stride : SignedInteger {
+    public typealias Position = RangePosition
+    
+    public func enumerateRanges(_anchoredAt position: Range.Position) -> [Self] {
+        let rangeLength = startIndex.distance(to: endIndex)
+        
+        switch position {
+        case .start:
+            return (1..<rangeLength).map {
+                let upperBound = startIndex.advanced(by: $0)
+                return (startIndex..<upperBound)
+            }
+            
+        case .end:
+            return (0..<rangeLength).map {
+                let lowerBound = startIndex.advanced(by: $0)
+                return (lowerBound..<upperBound)
+            }
+        }
+    }
+    
+}
