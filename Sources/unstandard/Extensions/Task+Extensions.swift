489365b101d38eae0b5d7081380ca8a5284ed868
Christopher Weems <hello@christopherweems.com>
2021-11-16 15:36:35 -0500

    Add `Task.sleep(_:)` for `TimeInterval` durations

---
+++ Sources/unstandard/Extensions/Task+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Task+Extensions.swift
+//  
+//
+//  Created by Christopher Weems on 11/16/21.
+//
+
+import struct Foundation.TimeInterval
+
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+extension Task where Success == Never, Failure == Never {
+    public static func sleep(_ sleepDuration: TimeInterval) async throws {
+        let nanoseconds = UInt64(sleepDuration * 1_000_000_000)
+        try await Task.sleep(nanoseconds: nanoseconds)
+    }
+    
+}
