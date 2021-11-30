ebfa146c89c8b47f48a1ffe9dc4ca7562203e63f
Christopher Weems <hello@christopherweems.com>
2021-11-30 14:10:45 -0500

    `EmptyBuilder` is now deprecated

--- Sources/unstandard/Result Builders/EmptyBuilder.swift
+++ Sources/unstandard/Result Builders/EmptyBuilder.swift
@@ -5,8 +5,7 @@
 //  Created by Christopher Weems on 4/4/20.
 //
 
-import Foundation
-
+@available(*, deprecated)
 @resultBuilder
 public struct EmptyBuilder {
     public static func buildBlock() -> Void { }
