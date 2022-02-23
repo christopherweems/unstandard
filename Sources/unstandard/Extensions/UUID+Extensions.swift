53aa64c8ae670380e559bb6607960e57b4dd042b
Christopher Weems <hello@christopherweems.com>
2022-02-22 20:52:19 -0500

    Add `UUID.bytes`

--- Sources/unstandard/Extensions/UUID+Extensions.swift
+++ Sources/unstandard/Extensions/UUID+Extensions.swift
@@ -15,6 +15,20 @@ extension UUID {
     
 }
 
+extension UUID {
+    public var bytes: [UInt8] {
+        let components = self.uuid
+        
+        return [
+            components.0, components.1, components.2, components.3,
+            components.4, components.5, components.6, components.7,
+            components.8, components.9, components.10, components.11,
+            components.12, components.13, components.14, components.15,
+        ]
+    }
+    
+}
+
 /// Usage: `UUID.String` can be used in declarations to specify what kind of String the function expects
 extension UUID {
     public typealias String = Swift.String
