b07f63f6905850b3b56d10ea4e7e62591f1cb316
Christopher Weems <hello@christopherweems.com>
2022-05-03 11:18:51 -0400

    Remove existing `StringProtocol.dropSuffix(..)` & replace with version with new type signature [source breaking change] Add `SringProtocol.droppingSuffix(..)`

--- Sources/unstandardStrings/StringProtocol/StringProtocol+AffixOperations.swift
+++ Sources/unstandardStrings/StringProtocol/StringProtocol+AffixOperations.swift
@@ -14,14 +14,14 @@ extension StringProtocol {
 }
 
 extension StringProtocol {
-    // TODO: - Fix discrepancy between API of `dropSuffix` & `dropPrefix`
     public mutating func dropPrefix<P>(_ prefix: P) where Self == SubSequence, P : StringProtocol {
-        precondition(self.hasPrefix(prefix))
+        precondition(self.hasPrefix(prefix), "\(self) does not have prefix \(prefix)")
         let newStartIndex = self.index(\.startIndex, offsetBy: prefix.count)
         self = self[newStartIndex...]
     }
     
     public func droppingPrefix<P>(_ prefix: P) -> SubSequence where P : StringProtocol {
+        precondition(self.hasPrefix(prefix), "\(self) does not have prefix \(prefix)")
         let newStartIndex = self.index(\.startIndex, offsetBy: prefix.count)
         return self[newStartIndex...]
     }
@@ -37,7 +37,25 @@ extension StringProtocol {
 }
 
 extension StringProtocol {
-    // TODO: - Fix discrepancy between API of `dropSuffix` & `dropPrefix`
+    public mutating func dropSuffix<S>(_ suffix: S) where Self == SubSequence, S : StringProtocol {
+        precondition(self.hasSuffix(suffix), "\(self) does not have suffix \(suffix)")
+        let newEndIndex = self.index(\.endIndex, offsetBy: -suffix.count)
+        self = self[..<newEndIndex]
+    }
+    
+    public func droppingSuffix<S>(_ suffix: S) -> SubSequence where S : StringProtocol {
+        precondition(self.hasSuffix(suffix), "\(self) does not have suffix \(suffix)")
+        let newEndIndex = self.index(\.endIndex, offsetBy: -suffix.count)
+        return self[..<newEndIndex]
+    }
+    
+}
+
+
+// MARK: - Deprecated API
+
+extension StringProtocol {
+    @available(*, unavailable)
     @discardableResult
     public mutating func dropSuffix(_ suffix: String) -> Bool {
         guard self.hasSuffix(suffix) else { return false }
