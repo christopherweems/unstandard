cc3d33036737f89f768d26f45b639835ea0cf561
Christopher Weems <hello@christopherweems.com>
2025-04-18 22:58:59 -0400

    Remove `CharacterSet.sentenceTerminatingPunctuation(in:)` [breaking]

--- Sources/unstandardStringsFoundation/CharacterSet/CharacterSet+SetDefinitions.swift
+++ Sources/unstandardStringsFoundation/CharacterSet/CharacterSet+SetDefinitions.swift
@@ -6,21 +6,10 @@
 //
 
 import Foundation
-import lc_locale
 
 extension CharacterSet {
     public static let hyphens: CharacterSet = {
         ["-"]
     }()
-    
-}
-
-extension CharacterSet {
-    public static func sentenceTerminatingPunctuation(in language: Locale.LCLanguage) -> Self {
-        switch language {
-        default:
-            return .init(charactersIn: ".!?")
-        }
-    }
     
 }
