69738198e8dbc09c506b64b6936c993d694fbcc2
Christopher Weems <hello@christopherweems.com>
2025-05-21 11:18:44 -0400

    Update minimum package dependency versions

--- Package.swift
+++ Package.swift
@@ -45,9 +45,9 @@ let package = Package(
         
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-algorithms.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.2"),
-        .package(url: "https://github.com/apple/swift-system.git", from: "1.3.2"),
+        .package(url: "https://github.com/apple/swift-algorithms.git", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.0"),
+        .package(url: "https://github.com/apple/swift-system.git", from: "1.4.0"),
         
     ],
     targets: [
